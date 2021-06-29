de126ba20d83c7b5f17b74d78b26e92f37032c86
Chris Berniel Cobashatse <cc4536@columbia.edu>
2021-06-29 21:18:14 +0000

    Modified style mistakes in sentiment.py and spotify.py

--- sentiment.py
+++ sentiment.py
@@ -5,6 +5,6 @@ sample_text = input('Enter a text: ')
 url = 'http://text-processing.com/api/sentiment/'
 myobj = {'text': sample_text}
 
-response = requests.post(url, data = myobj)
+response = requests.post(url, data=myobj)
 
 print(response.json())
--- spotify.py
+++ spotify.py
@@ -6,9 +6,9 @@ CLIENT_SECRET = 'c0598a796ad64991abae0be8b0fa1d55'
 AUTH_URL = 'https://accounts.spotify.com/api/token'
 
 auth_response_post = requests.post(AUTH_URL, {
-  'grant_type' : 'client_credentials',
-  'client_id' : CLIENT_ID,
-  'client_secret' : CLIENT_SECRET,
+  'grant_type': 'client_credentials',
+  'client_id': CLIENT_ID,
+  'client_secret': CLIENT_SECRET,
 })
 
 auth_response_post_data = auth_response_post.json()
@@ -22,9 +22,11 @@ BASE_URL = 'https://api.spotify.com/v1/'
 
 artist_id = '36QJpDe2go2KgaRleHCDTp'
 
-auth_response_get = requests.get(BASE_URL + 'artists/' + artist_id + '/albums', headers=headers, params={'include_groups' : 'album', 'limit': 1})
+auth_response_get = requests.get(BASE_URL + 'artists/' + artist_id +
+                                 '/albums', headers=headers,
+                                 params={'include_groups' : 'album', 'limit': 1})
 
-#convert the response to JSON
+# convert the response to JSON
 auth_response_get_data = auth_response_get.json()
 
 print(auth_response_get.status_code)
