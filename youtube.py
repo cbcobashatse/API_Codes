cd658838198dcd5c739f6619598d00755b3838cb
Chris Berniel Cobashatse <cc4536@columbia.edu>
2021-06-29 21:26:13 +0000

    Modified sentiment.py and youtube.py

--- youtube.py
+++ youtube.py
@@ -8,38 +8,3 @@ yt = YouTubeDataAPI(api_key)
 
 results = yt.search('bridge2rwanda')
 print(results)
-
-"""
-
-from pyyoutube import Api
-api = Api(api_key="AIzaSyC8ScUbhEYWOFOnsflsbmKng2_C0huR-g8")
-
-from youtube_videos.py import youtube_search
-import json
-
-test = youtube_search("spinners")
-test
-
-
-
-# Get authorization url
->>> api.get_authorization_url()
-('https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id
-=id&redirect_uri=https%3A%2F%2Flocalhost%2F&scope=scope&state=PyYouTube&
-access_type=offline&prompt=select_account', 'PyYouTube')
-# user to do
-# copy the response url
->>> api.generate_access_token(authorization_response="link for response")
-AccessToken(access_token='token', expires_in=3599, token_type='Bearer')
-
-
-
->>> video_by_id = api.get_video_by_id(video_id="CvTApw9X8aA")
-
->>> video_by_id
-VideoListResponse(kind='youtube#videoListResponse')
-
->>> video_by_id.items
-[Video(kind='youtube#video', id='CvTApw9X8aA')]
-
-"""
