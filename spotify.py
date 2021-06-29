3be6f002d776262130d9c435870cae3e1e7c15b2
Chris Berniel Cobashatse <cc4536@columbia.edu>
2021-06-29 19:48:16 +0000

    Added a GET request

--- spotify.py
+++ spotify.py
@@ -5,14 +5,27 @@ CLIENT_ID = '92e33f2cb60a4788be53e6f0352b00ac'
 CLIENT_SECRET = 'c0598a796ad64991abae0be8b0fa1d55'
 AUTH_URL = 'https://accounts.spotify.com/api/token'
 
-auth_response = requests.post(AUTH_URL, {
+auth_response_post = requests.post(AUTH_URL, {
   'grant_type' : 'client_credentials',
   'client_id' : CLIENT_ID,
   'client_secret' : CLIENT_SECRET,
 })
 
-#convert the response to JSON
-auth_response_data = auth_response.json()
+auth_response_post_data = auth_response_post.json()
 
-print(auth_response.status_code)
-print(auth_response_data)
+access_token = auth_response_post_data['access_token']
+headers = {
+        'Authorization' : 'Bearer {token}'.format(token=access_token)
+        }
+
+BASE_URL = 'https://api.spotify.com/v1/'
+
+artist_id = '36QJpDe2go2KgaRleHCDTp'
+
+auth_response_get = requests.get(BASE_URL + 'artists/' + artist_id + '/albums', headers=headers, params={'include_groups' : 'album', 'limit': 1})
+
+#convert the response to JSON
+auth_response_get_data = auth_response_get.json()
+
+print(auth_response_get.status_code)
+print(auth_response_get_data)
