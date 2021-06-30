8076d651477011705c35749577309457b2615c05
Chris Berniel Cobashatse <cc4536@columbia.edu>
2021-06-30 21:07:16 +0000

    Modified covid.py to load the API data into a database

--- covid.py
+++ covid.py
@@ -1,18 +1,45 @@
 import requests
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
 
+# the base URL to be used
 base_url = 'https://covid-api.mmediagroup.fr/v1'
 
-country = input('Enter a country: ')
+# ask the user for input
+user_country = input('Enter a country: ')
 
-response = requests.get(base_url + '/cases?country=' + country)
+# get the data from the API
+response = requests.get(base_url + '/cases?country=' + user_country)
 response_json = response.json()
 country_data = response_json['All']
 
-print(f'Covid Situation in {country_data["country"]}')
-print(f'Confirmed Cases : {country_data["confirmed"]}')
-print(f'Recovered : {country_data["recovered"]}')
-print(f'Deaths : {country_data["deaths"]}')
-print(f'Total Population : {country_data["population"]}')
+# data items to be used
+country = country_data["country"]
+confirmed = country_data["confirmed"]
+recovered = country_data["recovered"]
+deaths = country_data["deaths"]
+population = country_data["population"]
+
+# print statements
+print(f'Covid Situation in {country}')
+print(f'Confirmed Cases : {confirmed}')
+print(f'Recovered : {recovered}')
+print(f'Deaths : {deaths}')
+print(f'Total Population : {population}')
+
+# creating data frame from the dictionary obtained
+col_names = ['Country', 'Confirmed', 'Recovered', 'Deaths', 'Population']
+country_data_df = pd.DataFrame(columns = col_names)
+country_data_df.loc[len(country_data_df.index)] =  [country, confirmed, 
+                                                   recovered, deaths, 
+                                                   population]
+
+# Create an engine
+engine = create_engine('mysql://root:codio@localhost/covid')
+
+# Create and send SQLtable from my dataframe
+country_data_df.to_sql('by_country', con=engine, if_exists='replace', index=False)
 
 # Not every country has this value
 # print(f'Last updated at {country_data["updated"]}')
