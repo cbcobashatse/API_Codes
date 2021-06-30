d4e73315df10fdde64b9aa9293fd8fcd3489950d
Chris Berniel Cobashatse <cc4536@columbia.edu>
2021-06-30 21:56:37 +0000

    Corrected style errors in covid.py

--- covid.py
+++ covid.py
@@ -6,10 +6,11 @@ from sqlalchemy import create_engine
 # the base URL to be used
 base_url = 'https://covid-api.mmediagroup.fr/v1'
 
-# test 1: check that the user input is not empty
+# test 1: check that the user input is not empty and is a valid country
 def get_user_input():
     # ask the user for input
     user_country = input('Enter a country: ')
+
 
 # test 2: check that the status code is 200
 # test 3: check that the file obtained is in JSON
@@ -21,6 +22,7 @@ def get_data_from_api():
     response_json = response.json()
     country_data = response_json['All']
 
+
 # test 6: check that country is a string and the others are integers
 def get_data_items():
     # data items to be used
@@ -30,6 +32,7 @@ def get_data_items():
     deaths = country_data["deaths"]
     population = country_data["population"]
 
+
 # not sure what checks to use
 def print_data():
     # print statements
@@ -38,6 +41,7 @@ def print_data():
     print(f'Recovered : {recovered}')
     print(f'Deaths : {deaths}')
     print(f'Total Population : {population}')
+
 
 # test 7: check that col_names is a list of strings
 # test 8: check that country_data_df is a dataframe object
@@ -50,15 +54,17 @@ def dict_to_df():
                                                        recovered, deaths,
                                                        population]
 
+
 # test 10: check that the engine has been created
 # test 11: check that the SQL table has been created
 def engine_and_SQLtable():
     # Create an engine
     engine = create_engine('mysql://root:codio@localhost/covid')
-    
+
     # Create and send SQLtable from my dataframe
     SQLtable = country_data_df.to_sql('by_country', con=engine,
                                       if_exists='replace', index=False)
+
 
 # Not every country has this value
 # print(f'Last updated at {country_data["updated"]}')
